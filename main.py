d8242496376a502d8aff46b3a1a55d94bda1c59d
Nick Adair <adair8@purdue.edu>
2023-06-08 11:27:53 -0400

    could be min till I see python again lol so many things for work

--- main.py
+++ main.py
@@ -1,36 +1,71 @@
 import random
+from enum import Enum
 
-def get_choices():
-    player_choice = input("Enter a choice for rock-paper-scissors: ")
-    options = ["rock","paper","scissors"]
-    computer_choice = random.choice(options)
-    choices = {"player": player_choice, "computer": computer_choice}
-    return choices
+#Tuples
+names = ("Nick", "Will")
+names[0]
+names.index("Nick")
 
-def check_win(player, computer):
-    print(f"you chose:  {player}  computer chose:   {computer}")
-    if(player == computer):
-        return "It's a tie" 
-    elif(player == "rock" ):
-        if(computer == "scissors"):
-            return "you win"
-        else:
-            return "you lose"
-    elif(player == "paper" ):
-        if(computer == "rock"):
-            return "you win"
-        else:
-            return "you lose"
-    elif(player == "scissors" ):
-        if(computer == "paper"):
-            return "you win"
-        else:
-            return "you lose"
+pri
+
+# lists
+# dogs = ["Malibu", "Marley", "Mae Mo"]
+# humans = ["nick", "will"]
+# dogs+=humans
+# dogs.remove("will")
+# dogs[1:1] = ["Test1", "Test2"]
+# print(sorted(dogs,key=str.lower))
+# dogsCopy = dogs[:]
+# print(dogs)
+# print(dogsCopy)
+
+#ENUMS
+# class State(Enum):
+#     INACTIVE = 0
+#     ACTIVE = 1
+
+# print(len(State))
+
+
+# name = "nick is cool"
+# last = ""
+
+# full_name = all([name, last])
+# print(full_name)
+
+
+
+# def get_choices():
+#     player_choice = input("Enter a choice for rock-paper-scissors: ")
+#     options = ["rock","paper","scissors"]
+#     computer_choice = random.choice(options)
+#     choices = {"player": player_choice, "computer": computer_choice}
+#     return choices
+
+# def check_win(player, computer):
+#     print(f"you chose:  {player}  computer chose:   {computer}")
+#     if(player == computer):
+#         return "It's a tie" 
+#     elif(player == "rock" ):
+#         if(computer == "scissors"):
+#             return "you win"
+#         else:
+#             return "you lose"
+#     elif(player == "paper" ):
+#         if(computer == "rock"):
+#             return "you win"
+#         else:
+#             return "you lose"
+#     elif(player == "scissors" ):
+#         if(computer == "paper"):
+#             return "you win"
+#         else:
+#             return "you lose"
 
     
-choices = get_choices()
-result = check_win(choices["player"], choices["computer"])
-print(result)
+# choices = get_choices()
+# result = check_win(choices["player"], choices["computer"])
+# print(result)
 
 
 # def greeting():
